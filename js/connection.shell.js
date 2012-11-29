c05f26ac29027a7fce02296200093a55d21bf959
Chris Lane <chris@chris-allen-lane.com>
2012-11-28 20:50:54 -0500

    Refactoring away redundancy among the JavaScript files.

--- js/connection.shell.js
+++ js/connection.shell.js
@@ -1,14 +1,5 @@
 // encapsulates connectioning functionality
 shell.connection = {
-    // clears the connection
-    clear: function(){
-        shell.debug.log('shell.connection.clear');
-        shell.connection.url      = '',
-        shell.connection.password = '',
-        $(shell.elements.url).val('');
-        $(shell.elements.password).val('');
-    },
-
     // hides the connection bar
     hide: function(){
         shell.debug.log('shell.connection.hide');
@@ -18,27 +9,12 @@ shell.connection = {
         });
     },
 
-    // clears the connection
-    set: function(){
-        shell.debug.log('shell.connection.set');
-        shell.connection.protocol = $(shell.elements.protocol).val();
-        shell.connection.url      = $(shell.elements.url).val();
-        shell.connection.port     = $(shell.elements.port).val();
-        shell.connection.password = $(shell.elements.password).val();
-    },
-
     // un-hides the connection bar
     show: function(){
         shell.debug.log('shell.connection.show');
         $(shell.elements.connection).fadeIn(function(){
             shell.status.set('connection bar revealed. Press Ctrl+h to hide.');
         });
-    },
-
-    // tests the connection to the connection
-    test: function(){
-        shell.debug.log('shell.connection.test');
-        //@TODO: implement this
     },
 
     // toggles connection bar visibility
