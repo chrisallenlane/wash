e0511eeb9b36a3d317e8dd8b5a64a5d755bfa544
Chris Lane <chris@chris-allen-lane.com>
2012-11-28 19:12:59 -0500

    Refactoring

--- js/shell.js
+++ js/shell.js
@@ -39,29 +39,6 @@ var shell = {
         this.prompt.draw();
     },
 
-    // encapsulates the status bar
-    status: {
-
-        // appends to the status bar
-        append: function(data){
-            shell.debug.log('shell.status.append');
-            $(shell.elements.status).append(data);
-        },
-
-        // clears the status bar
-        clear: function(){
-            shell.debug.log('shell.status.clear');
-            $(shell.elements.status).text('');
-        },
-
-        // sets the status bar message
-        set: function(data){
-            shell.debug.log('shell.status.set');
-            $(shell.elements.status).text(data).text();
-        },
-
-    },
-
     // encapsulates targeting functionality
     target: {
 
