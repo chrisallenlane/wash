5621f400ce6ef53a8c022ab972bd6c536e9ed072
Chris Lane <chris@chris-allen-lane.com>
2012-12-02 14:51:42 -0500

    Resolved issue #20. While I didn't change the User Interface, I'm now parsing the domain out of the path from the 'url' field, and storing each separately in the wash.connection object.

--- js/connection.wash.js
+++ js/connection.wash.js
@@ -1,8 +1,9 @@
 // manage connections
 wash.connection = {
     // @todo: I'm hard-coding these while debugging
+    domain       : 'wash',
     protocol     : 'http',
-    url          : 'wash/trojans/plaintext/trojan.php',
+    url          : '/trojans/plaintext/trojan.php',
     port         : '80',
     password     : '',
     request_type : 'post',
@@ -10,6 +11,32 @@ wash.connection = {
     // loads a saved connection
     load: function(){
         console.log('TODO: implement this.');
+    },
+
+    // parses a url into a domain and path
+    parse_url: function(url){
+        // initialize some vars for parsing
+        var domain, path; domain = path = '';
+        var first_slash_position = url.indexOf('/');
+
+        // if the url contains no path, manually specify path as /
+        if(first_slash_position  == -1){
+            domain = url;
+            path   = '/';
+        }
+
+        // otherwise, parse
+        else{
+            domain = url.substr(0, first_slash_position);
+            path   = url.substr(first_slash_position);
+        }
+        
+        // structure and return the result
+        var parts = {
+            domain : domain,
+            path   : path,
+        }
+        return parts;
     },
 
     // saves a connection
@@ -24,7 +51,22 @@ wash.connection = {
 
     // updates a connection parameter
     set: function(parameter, value){
-        wash.connection[parameter] = value;
-        shell.status.set('wash.connection.' + parameter + ' has been set to ' + value + '.');
+
+        // if the URL is being updated, parse out the domain first
+        if(parameter == 'url'){
+            var url_parts = this.parse_url(value);
+            wash.connection.domain = url_parts.domain;
+            wash.connection.path   = url_parts.path;
+            shell.status.set(
+                'wash.connection.domain has been set to ' + url_parts.domain + '. ' + 
+                'wash.connection.path has been set to '   + url_parts.path   + '.'
+            );
+        }
+
+        // otherwise, just set the parameter as a scalar
+        else {
+            wash.connection[parameter] = value;
+            shell.status.set('wash.connection.' + parameter + ' has been set to ' + value + '.');
+        }
     },
 }
