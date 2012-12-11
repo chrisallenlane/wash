712d2c4cd338b60e1dbcc187b8d4addd6913f284
Chris Lane <chris@chris-allen-lane.com>
2012-12-10 20:03:41 -0500

    Took the first stab at getting a sensible help system in place. I've got something working now, though it doesn't feel architecturally perfect yet.

--- extensions/wash.file.js
+++ extensions/wash.file.js
@@ -2,6 +2,24 @@
 // functionality.
 wash.file = {
 
+    // the help text
+    help: function(args){
+        // push the help objects onto the array
+        var help_objects = [];
+        help_objects.push({
+            command : "wash.file.down({file: 'the-file.txt'})",
+            text    : "Downloads the specified file.",
+        })
+        help_objects.push({
+            command : "wash.file.up()",
+            text    : "Prompts the user to upload a file.",
+        })
+
+        // output or return the help objects appropriately
+        if(args != null && args.return === true){ return help_objects; }
+        else { wash.help_display(help_objects); }
+    },
+
     // downloads a file
     down: function(args){
         // alert the user if we're switching to GET from another request type
@@ -32,8 +50,6 @@ wash.file = {
 
     // uploads files to the target server
     up: function(){
-        console.log('up');
-
         // alert the user if we're switching to POST from another request type
         if(wash.connection.request_type != 'post'){
             var msg = "In order to download a file, a POST request must necessarily " +
