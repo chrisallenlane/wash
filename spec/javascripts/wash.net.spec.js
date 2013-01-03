2608f6b9a8ac986ef7b2fdefc61d9486995edc6a
Chris Lane <chris@chris-allen-lane.com>
2013-01-03 15:36:25 -0500

    Writing unit tests.

--- spec/javascripts/wash.net.spec.js
+++ spec/javascripts/wash.net.spec.js
@@ -1,27 +1,22 @@
 describe("wash.net", function() {
     var request;
 
+    /*
     beforeEach(function() {
         loadFixtures('index.html');
         shell.init();
-
         jasmine.Ajax.useMock();
     });
-
-    /*
-    it("get() should invoke a custom callback", function() {
-        request = mostRecentAjaxRequest();
-        request.response(test_responses.dummy.success);
-        console.log(request);
-
-        //runs(function(){
-            wash.net.get(function(response){
-                console.log('I was called!');
-                console.log(response);
-            });
-        //});
-    });
     */
+
+    /**
+     * @bug @kludge: I can't seem to write successful tests for wash.net.get
+     * and wash.net.send, because the .done() chained method does not seem to
+     * be firing. My best guess is that the AJAX mocking library simply does not
+     * support this. The only good news here is that bugs in either of those
+     * two methods are likely to be extremely conspicuous, so are unlikely
+     * to go unnoticed until I figure out a way to resolve this problem.
+     */
 
     it("should receive inbound communications", function(){
         wash.net.receive('{"output":"mock output"}', function(){return void(0);});
